9b9eb63d44ea81fd93206fed4eccabf916533a67
Redstoneboi <raffimolero@gmail.com>
2023-08-21 23:01:05 +0800

    highlight selected entity

--- examples/metacam.rs
+++ examples/metacam.rs
@@ -1,7 +1,6 @@
 use std::f32::consts::TAU;
 
 use bevy::{
-    ecs::system::EntityCommands,
     prelude::*,
     render::{
         camera::RenderTarget,
@@ -18,45 +17,21 @@ pub fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(Controlled::default())
+        .insert_resource(GizmoConfig {
+            line_width: 4.0,
+            ..default()
+        })
         .add_systems(Startup, setup)
-        .add_systems(Update, (close_on_esc, control_selection, control_movement))
+        .add_systems(
+            Update,
+            (
+                close_on_esc,
+                control_selection,
+                control_movement,
+                aabb_rainbow,
+            ),
+        )
         .run()
-}
-
-#[derive(Resource, Default)]
-struct Controlled {
-    entities: Vec<Entity>,
-    selected: usize,
-}
-
-impl Controlled {
-    fn select<'a>(
-        &self,
-        controllables: &'a mut Query<&mut Transform>,
-    ) -> Option<Mut<'a, Transform>> {
-        self.entities
-            .get(self.selected)
-            .and_then(|entity| controllables.get_mut(*entity).ok())
-    }
-
-    fn select_next(&mut self) {
-        self.selected += 1;
-        // not a modulo, in case that entities are removed
-        if self.selected >= self.entities.len() {
-            self.selected = 0;
-        }
-    }
-
-    fn select_prev(&mut self) {
-        if self.selected == 0 {
-            self.selected = self.entities.len();
-        }
-        self.selected -= 1;
-    }
-
-    fn push(&mut self, entity: Entity) {
-        self.entities.push(entity)
-    }
 }
 
 fn setup(
@@ -68,7 +43,6 @@ fn setup(
     let h: u32 = 256;
 
     let (subwindow, sublayer) = spawn_subwindow(w, h, &mut textures, &mut commands);
-    let subwindow = subwindow.id();
 
     let main_cam = commands.spawn(Camera2dBundle::default()).id();
 
@@ -90,14 +64,92 @@ fn setup(
     controlled.push(subwindow);
     controlled.push(rectangle);
     controlled.push(main_cam);
+    controlled.attach_aabb(&mut commands);
 }
 
-fn control_selection(keys: Res<Input<KeyCode>>, mut controlled: ResMut<Controlled>) {
+#[derive(Resource, Default)]
+struct Controlled {
+    entities: Vec<Entity>,
+    selected: usize,
+}
+
+impl Controlled {
+    fn select<'a>(
+        &self,
+        controllables: &'a mut Query<&mut Transform>,
+    ) -> Option<Mut<'a, Transform>> {
+        self.entities
+            .get(self.selected)
+            .and_then(|entity| controllables.get_mut(*entity).ok())
+    }
+
+    fn detach_aabb(&self, commands: &mut Commands) {
+        let Some(&entity) = self.entities.get(self.selected) else {
+            return;
+        };
+        commands.entity(entity).remove::<AabbGizmo>();
+    }
+
+    fn attach_aabb(&self, commands: &mut Commands) {
+        let Some(&entity) = self.entities.get(self.selected) else {
+            return;
+        };
+        commands.entity(entity).insert(AabbGizmo::default());
+    }
+
+    fn select_next(&mut self, commands: &mut Commands) {
+        self.detach_aabb(commands);
+        self.selected += 1;
+        if self.selected >= self.entities.len() {
+            self.selected = 0;
+        }
+        self.attach_aabb(commands);
+    }
+
+    fn select_prev(&mut self, commands: &mut Commands) {
+        self.detach_aabb(commands);
+        if self.selected == 0 {
+            self.selected = self.entities.len();
+        }
+        self.selected -= 1;
+        self.attach_aabb(commands);
+    }
+
+    fn push(&mut self, entity: Entity) {
+        self.entities.push(entity)
+    }
+}
+
+fn aabb_rainbow(time: Res<Time>, mut config: ResMut<GizmoConfig>) {
+    match &mut config.aabb.default_color {
+        None => {
+            *(&mut config.aabb.default_color) = Some(Color::Hsla {
+                hue: 0.0,
+                saturation: 1.0,
+                lightness: 0.5,
+                alpha: 0.75,
+            })
+        }
+        Some(Color::Hsla { hue, .. }) => {
+            *hue += time.delta_seconds() * 60.0;
+            *hue %= 360.0;
+        }
+        Some(col) => {
+            *col = col.as_hsla();
+        }
+    }
+}
+
+fn control_selection(
+    keys: Res<Input<KeyCode>>,
+    mut controlled: ResMut<Controlled>,
+    mut commands: Commands,
+) {
     if keys.just_pressed(KeyCode::O) {
-        controlled.select_next();
+        controlled.select_next(&mut commands);
     }
     if keys.just_pressed(KeyCode::U) {
-        controlled.select_prev();
+        controlled.select_prev(&mut commands);
     }
 }
 
@@ -108,8 +160,8 @@ fn control_movement(
     mut controllables: Query<&mut Transform>,
 ) {
     let delta = time.delta_seconds();
-    let speed = 400.0;
-    let rot = TAU / 2.0;
+    let speed = 300.0;
+    let rot = TAU / 4.0;
     let scaling = 4_f32;
 
     let mut control_tf = Transform::IDENTITY;
@@ -149,46 +201,48 @@ fn control_movement(
     }
 }
 
-fn spawn_subwindow<'w, 's, 'a>(
+fn spawn_subwindow(
     w: u32,
     h: u32,
     textures: &mut ResMut<Assets<Image>>,
-    commands: &'a mut Commands<'w, 's>,
-) -> (EntityCommands<'w, 's, 'a>, RenderLayers) {
+    commands: &mut Commands,
+) -> (Entity, RenderLayers) {
     // TODO: auto generate this based on available layers
-    let layer = RenderLayers::layer(1);
+    let sublayer = RenderLayers::layer(1);
 
     let img = new_image(w, h);
     let image_handle = textures.add(img);
 
-    let container = SpriteBundle {
-        sprite: Sprite {
-            color: Color::ALICE_BLUE,
-            custom_size: Some(Vec2::new(w as f32 * 1.25, h as f32 * 1.25)),
+    let container = commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::ALICE_BLUE,
+                custom_size: Some(Vec2::new(w as f32 * 1.25, h as f32 * 1.25)),
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    };
-    let texture_sprite = SpriteBundle {
-        texture: image_handle.clone(),
-        transform: Transform::from_xyz(0.0, 0.0, 1.0),
-        ..default()
-    };
-    let camera = Camera2dBundle {
-        camera: Camera {
-            target: RenderTarget::Image(image_handle.clone()),
-            ..default()
-        },
-        ..default()
-    };
+        })
+        .with_children(|parent| {
+            parent.spawn((
+                Camera2dBundle {
+                    camera: Camera {
+                        target: RenderTarget::Image(image_handle.clone()),
+                        order: -1,
+                        ..default()
+                    },
+                    ..default()
+                },
+                sublayer,
+            ));
+            parent.spawn(SpriteBundle {
+                texture: image_handle.clone(),
+                transform: Transform::from_xyz(0.0, 0.0, 1.0),
+                ..default()
+            });
+        })
+        .id();
 
-    let mut container = commands.spawn(container);
-    container.with_children(|parent| {
-        parent.spawn(texture_sprite).with_children(|parent| {
-            parent.spawn((camera, layer));
-        });
-    });
-    (container, layer)
+    (container, sublayer)
 }
 
 fn new_image(w: u32, h: u32) -> Image {
